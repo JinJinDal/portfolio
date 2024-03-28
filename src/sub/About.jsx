47ddedd589c9fc9fff08cbc3ea28ae0034d72721
KimJinWoo <krlawls1@gmail.com>
2024-03-28 14:33:55 +0900

    03281433

--- src/sub/About.jsx
+++ src/sub/About.jsx
@@ -1,15 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import dataJson from '../data/skill-list.json'
 
 
 
 function About(props) {
 
+    const [testY, setTestY] = useState(null);
+    const [isOverStack, setIsOverStack] = useState(false);
+    const aboutSectionRef = useRef(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (aboutSectionRef.current) {
+                const rect = aboutSectionRef.current.getBoundingClientRect();
+                const giveClass = document.querySelectorAll('.stack')
+                // console.log(rect.top); // top 위치 출력
+                if (rect.top < 1) {
+                    setIsOverStack(true);
+                    console.log('ok')
+                } else {
+                    setIsOverStack(false);
+                }
+                // 이제 top 위치에 따른 원하는 동작을 여기에 추가할 수 있습니다.
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+
     const renders = (items) => {
         const techEtc = items;
         return techEtc.map((item) => (
             <li key={item.id}>
-                <div className='stack'>
+                <div className={`stack ${isOverStack ? 'overStack' : ''}`}>
                     <div className='stackDt'>
                         <div className='stackImg'>
                             <img src={item.img} />  
@@ -24,9 +52,11 @@ function About(props) {
         ))
     }
 
+    
+
     return (
         <>
-            <section className='sct2' id='About'>
+            <section ref={aboutSectionRef} className='sct2' id='About'>
                 <div className='sct2-content2'>
                     <h2>이런걸 할 수있는 <span className='permanent'>me !</span></h2>
                     <div>
